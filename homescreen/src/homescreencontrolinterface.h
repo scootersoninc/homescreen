52ef216acfe810a482dbab0c4422dad6ad8e77b7
Marcus Fritzsch <marcus_fritzsch@mentor.com>
2017-05-10 14:10:05 +0200

    HomeScreenControlInterface: use single afm_user_daemon
    * The org.agl.homescreenappframeworkbindertizen should not be used here.
    * Simplify the implementation.
    
    Change-Id: I5d2dedf2dcacea2104ba38e4467fd8299a88d776
    Signed-off-by: Marcus Fritzsch <marcus_fritzsch@mentor.com>

--- homescreen/src/homescreencontrolinterface.h
+++ homescreen/src/homescreencontrolinterface.h
@@ -20,15 +20,12 @@
 #include <QObject>
 #include "include/homescreen.hpp"
 #include "homescreen_adaptor.h"
-#include <include/appframework.hpp>
-#include <appframework_proxy.h>
 
 class HomeScreenControlInterface : public QObject
 {
     Q_OBJECT
 public:
     explicit HomeScreenControlInterface(QObject *parent = 0);
-    ~HomeScreenControlInterface();
 
 signals:
     void newRequestsToBeVisibleApp(int pid);
@@ -47,9 +44,9 @@ public Q_SLOTS: // METHODS
     void renderSurfaceToArea(int surfaceId, int layoutArea);
     bool renderAppToAreaAllowed(int appCategory, int layoutArea);
     void requestSurfaceIdToFullScreen(int surfaceId);
+
 private:
     HomescreenAdaptor *mp_homeScreenAdaptor;
-    org::agl::appframework *mp_dBusAppFrameworkProxy;
 };
 
 #endif // HOMESCREENCONTROLINTERFACE_H
