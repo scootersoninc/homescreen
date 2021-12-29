a920d9537132f3778c137edee8d6741102919787
Marius Vlad <marius.vlad@collabora.com>
2021-12-29 16:09:09 +0200

    homescreenhandler: Remove old artifacts
    We're no longer using anything from the older appfw so remove dead code.
    
    Bug-AGL: SPEC-4204
    Signed-off-by: Marius Vlad <marius.vlad@collabora.com>
    Change-Id: I8a9714701a9bbb0da6768cd4bcbca52cb23464cd

--- homescreen/src/homescreenhandler.cpp
+++ homescreen/src/homescreenhandler.cpp
@@ -38,19 +38,10 @@ HomescreenHandler::HomescreenHandler(Shell *_aglShell, ApplicationLauncher *laun
 
 HomescreenHandler::~HomescreenHandler()
 {
-#if 0
-    if (mp_hs != NULL) {
-        delete mp_hs;
-    }
-#endif
 }
 
 void HomescreenHandler::init(void)
 {
-#if 0
-    mp_hs = new LibHomeScreen();
-    mp_hs->init(port, token);
-#endif
     myThis = this;
 
     /*
@@ -61,45 +52,6 @@ void HomescreenHandler::init(void)
     connect(applaunch_iface, SIGNAL(started(QString)), this, SLOT(appStarted(QString)));
     connect(applaunch_iface, SIGNAL(terminated(QString)), this, SLOT(appTerminated(QString)));
 
-#if 0
-    mp_hs->registerCallback(nullptr, HomescreenHandler::onRep_static);
-
-    mp_hs->set_event_handler(LibHomeScreen::Event_OnScreenMessage, [this](json_object *object){
-        const char *display_message = json_object_get_string(
-            json_object_object_get(object, "display_message"));
-        HMI_DEBUG("HomeScreen","set_event_handler Event_OnScreenMessage display_message = %s", display_message);
-    });
-
-    // should be handled in the top panel
-    mp_hs->set_event_handler(LibHomeScreen::Event_ShowNotification,[this](json_object *object){
-       json_object *p_obj = json_object_object_get(object, "parameter");
-       const char *icon = json_object_get_string(
-                   json_object_object_get(p_obj, "icon"));
-       const char *text = json_object_get_string(
-                   json_object_object_get(p_obj, "text"));
-       const char *app_id = json_object_get_string(
-                   json_object_object_get(p_obj, "caller"));
-       HMI_DEBUG("HomeScreen","Event_ShowNotification icon=%s, text=%s, caller=%s", icon, text, app_id);
-       QFileInfo icon_file(icon);
-       QString icon_path;
-       if (icon_file.isFile() && icon_file.exists()) {
-           icon_path = QString(QLatin1String(icon));
-       } else {
-           icon_path = "./images/Utility_Logo_Grey-01.svg";
-       }
-
-       emit showNotification(QString(QLatin1String(app_id)), icon_path, QString(QLatin1String(text)));
-    });
-
-    // should be handled in the bottom panel
-    mp_hs->set_event_handler(LibHomeScreen::Event_ShowInformation,[this](json_object *object){
-       json_object *p_obj = json_object_object_get(object, "parameter");
-       const char *info = json_object_get_string(
-                   json_object_object_get(p_obj, "info"));
-
-       emit showInformation(QString(QLatin1String(info)));
-    });
-#endif
 }
 
 static struct wl_output *
@@ -111,27 +63,7 @@ getWlOutput(QPlatformNativeInterface *native, QScreen *screen)
 
 void HomescreenHandler::tapShortcut(QString application_id)
 {
-	HMI_DEBUG("HomeScreen","tapShortcut %s", application_id.toStdString().c_str());
-#if 0
-	struct json_object* j_json = json_object_new_object();
-	struct json_object* value;
-
-	struct agl_shell *agl_shell = aglShell->shell.get();
-	QPlatformNativeInterface *native = qApp->platformNativeInterface();
-	struct wl_output *output = getWlOutput(native, qApp->screens().first());
-
-	value = json_object_new_string("normal.full");
-	json_object_object_add(j_json, "area", value);
-
-	mp_hs->showWindow(application_id.toStdString().c_str(), j_json);
-
-	// this works (and it is redundant the first time), due to the default
-	// policy engine installed which actives the application, when starting
-	// the first time. Later calls to HomescreenHandler::tapShortcut will
-	// require calling 'agl_shell_activate_app'
-	agl_shell_activate_app(agl_shell, application_id.toStdString().c_str(), output);
-
-#endif
+    HMI_DEBUG("HomeScreen","tapShortcut %s", application_id.toStdString().c_str());
 
     QDBusPendingReply<> reply = applaunch_iface->start(application_id);
     reply.waitForFinished();
@@ -146,38 +78,6 @@ void HomescreenHandler::tapShortcut(QString application_id)
         appStarted(application_id);
     }
 }
-
-#if 0
-void HomescreenHandler::onRep_static(struct json_object* reply_contents)
-{
-    static_cast<HomescreenHandler*>(HomescreenHandler::myThis)->onRep(reply_contents);
-}
-
-void HomescreenHandler::onEv_static(const string& event, struct json_object* event_contents)
-{
-    static_cast<HomescreenHandler*>(HomescreenHandler::myThis)->onEv(event, event_contents);
-}
-
-void HomescreenHandler::onRep(struct json_object* reply_contents)
-{
-    const char* str = json_object_to_json_string(reply_contents);
-    HMI_DEBUG("HomeScreen","HomeScreen onReply %s", str);
-}
-
-void HomescreenHandler::onEv(const string& event, struct json_object* event_contents)
-{
-    const char* str = json_object_to_json_string(event_contents);
-    HMI_DEBUG("HomeScreen","HomeScreen onEv %s, contents: %s", event.c_str(), str);
-
-    if (event.compare("homescreen/on_screen_message") == 0) {
-        struct json_object *json_data = json_object_object_get(event_contents, "data");
-        struct json_object *json_display_message = json_object_object_get(json_data, "display_message");
-        const char* display_message = json_object_get_string(json_display_message);
-
-        HMI_DEBUG("HomeScreen","display_message = %s", display_message);
-    }
-}
-#endif
 
 void HomescreenHandler::appStarted(const QString& application_id)
 {
